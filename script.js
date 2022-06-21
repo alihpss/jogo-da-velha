4d98d4bd1af714301a607750f1660449b5c63e02
alihpss <ali.hps@hotmail.com>
2022-06-21 17:50:59 -0300

    atualizando codigo

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-let conta = 0;
+let conta = 1;
 
 let turno = document.getElementById('turno');
 
 let res = document.getElementById('res')
 
-let blocos = [
-document.getElementById('div1'),
-document.getElementById('div2'),
-document.getElementById('div3'),
-document.getElementById('div4'),
-document.getElementById('div5'),
-document.getElementById('div6'),
-document.getElementById('div7'),
-document.getElementById('div8'),
-document.getElementById('div9')
-]; 
+let caixas = document.querySelectorAll('.quadrados');
 
 turno.textContent = ('Vez do jogador: X')
-
 
 function definirTurno(conta) {
     if(conta % 2 == 0) {
@@ -29,20 +18,33 @@ function definirTurno(conta) {
     }
 }
 
-blocos.forEach(function(bloco){
-    bloco.onclick = function () {
-        valorDoBloco(bloco);
+caixas.forEach(caixa => {
+    caixa.addEventListener('click', function (){
+        valorDoBloco(caixa);
         conta++
-    }
+    })
 })
+
+function valorDoBloco (atual){
+    if (definirTurno(conta)){
+        atual.innerText = ('o')
+    } else {
+        atual.innerText = ('x')
+    }
+
+    if (res.innerText != 'Jogador: X venceu!' & res.innerText != 'Jogador: O venceu!' & res.innerText != 'Empate!'){
+    resultado(caixas[0], caixas[1], caixas[2], caixas[3], caixas[4], caixas[5], caixas[6], caixas[7], caixas[8])
+    }
+    atual.classList.add('disabledbutton')
+}
 
 function reset () {
     if (conta > 0){
 
-        conta = 0
+        conta = 1
 
-        for ( let i = 0; i < blocos.length; i++){
-            limparBlocos(blocos[i])
+        for ( let i = 0; i < caixas.length; i++){
+            limparBlocos(caixas[i])
         }
 
         turno.textContent = ('Vez do jogador: X')
@@ -51,12 +53,12 @@ function reset () {
 }
 
 function resultado (a1, a2, a3, b1, b2, b3, c1, c2, c3) {
-    if (a1.innerText == 'x' && a2.innerText == 'x' && a3.innerText == 'x' || b1.innerText == 'x' && b2.innerText == 'x' & b3.innerText == 'x' || c1.innerText == 'x' && c2.innerText == 'x' && c3.innerText == 'x' || a1.innerText == 'x' && b1.innerText == 'x' && c1.innerText == 'x' || a2.innerText == 'x' && b2.innerText == 'x' && c2.innerText == 'x' || a3.innerText == 'x' && b3.innerText == 'x' && c3.innerText == 'x' || a1.innerText == 'x' && b2.innerText == 'x' && c3.innerText == 'x' || a3.innerText == 'x' && b2.innerText == 'x' && c1.innerText == 'x'){
+    if (verificarResultadoX(a1,a2,a3) || verificarResultadoX(b1,b2,b3)|| verificarResultadoX(c1,c2,c3) || verificarResultadoX(a1,b1,c1) ||verificarResultadoX(a2,b2,c2) || verificarResultadoX(a3,b3,c3)|| verificarResultadoX(a1,b2,c3)|| verificarResultadoX(a3,b2,c1)){
 
         res.innerText = ('Jogador: X venceu!')
         res.style.background = '#4cb359'
 
-    } else if (a1.innerText == 'o' && a2.innerText == 'o' && a3.innerText == 'o' || b1.innerText == 'o' && b2.innerText == 'o' & b3.innerText == 'o' || c1.innerText == 'o' && c2.innerText == 'o' && c3.innerText == 'o' || a1.innerText == 'o' && b1.innerText == 'o' && c1.innerText == 'o' || a2.innerText == 'o' && b2.innerText == 'o' && c2.innerText == 'o' || a3.innerText == 'o' && b3.innerText == 'o' && c3.innerText == 'o' || a1.innerText == 'o' && b2.innerText == 'o' && c3.innerText == 'o' || a3.innerText == 'o' && b2.innerText == 'o' && c1.innerText == 'o') {
+    } else if (verificarResultadoO(a1,a2,a3) || verificarResultadoO(b1,b2,b3)|| verificarResultadoO(c1,c2,c3) || verificarResultadoO(a1,b1,c1) ||verificarResultadoO(a2,b2,c2) || verificarResultadoO(a3,b3,c3)|| verificarResultadoO(a1,b2,c3)|| verificarResultadoO(a3,b2,c1)) {
 
         res.innerText = ('Jogador: O venceu!')
         res.style.background = '#4180bb'
@@ -67,23 +69,28 @@ function resultado (a1, a2, a3, b1, b2, b3, c1, c2, c3) {
     }
 }
 
-function valorDoBloco (atual){
-    if (definirTurno(conta)){
-        atual.innerText = ('o')
-    } else {
-        atual.innerText = ('x')
+function verificarResultadoO (item1, item2, item3) {
+    if (item1.innerText == 'o' && item2.innerText == 'o' && item3.innerText == 'o') {
+        return true
     }
+}
 
-    if (res.innerText != 'Jogador: X venceu!' & res.innerText != 'Jogador: O venceu!' & res.innerText != 'Empate!'){
-    resultado(blocos[0], blocos[1], blocos[2], blocos[3], blocos[4], blocos[5], blocos[6], blocos[7], blocos[8])
+function verificarResultadoX (item1, item2, item3) {
+    if (item1.innerText == 'x' && item2.innerText == 'x' && item3.innerText == 'x') {
+        return true
     }
-    atual.setAttribute('class', 'disabledbutton')
 }
 
 function limparBlocos (bloco){
     bloco.innerHTML = ''
-    bloco.setAttribute('class', 'quadrados')
+    let caixasTravadas = document.querySelectorAll('.disabledbutton')
 
+    for (let index = 0; index < caixasTravadas.length; index++) {
+        const caixaTravada = caixasTravadas[index];
+
+        caixaTravada.classList.remove('disabledbutton')
+        
+    }
     return limparBlocos
 }
 
